7259ef2f1cc5ca82bb79823a42e9410b115cab87
venkatsaikrishna <saikrishna.cherukuri9@gmail.com>
2022-03-29 16:16:47 +0530

    created a maven java project named helloworld

---
+++ helloworld/src/main/java/com/vsk_java_project/App.java
@@ -0,0 +1,13 @@
+package com.vsk_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
